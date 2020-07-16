be32e4a66797176def274dca6e0a5a6d85186ab4
Aaron Boodman <aaron@aaronboodman.com>
2020-07-16 11:14:58 -1000

    make it callable mult times

--- src/util/bench/mod.rs
+++ src/util/bench/mod.rs
@@ -1,6 +1,7 @@
 use async_std::future::Future;
 use async_std::task;
 use log::info;
+use std::pin::Pin;
 use std::time::Duration;
 
 fn now_nanos() -> u64 {
@@ -12,13 +13,15 @@ fn now_nanos() -> u64 {
 }
 
 pub async fn snooze() {
-    task::sleep(Duration::from_millis(10)).await;
+    task::sleep(Duration::from_millis(42)).await;
 }
 
-pub async fn drive(target: impl Future<Output = ()>) {
-    let now = now_nanos();
-    target.await;
-    let elapsed = now_nanos() - now;
-    // TODO: not sure how to see output -- info! compiles and runs but i don't know where it goes to under wasm-pack test
-    panic!("took {:.3}s", elapsed as f64 / 1e9);
+pub async fn drive<Fut: Future<Output = ()>>(target: &dyn Fn() -> Fut) {
+    for _ in 0u8..100 {
+        let now = now_nanos();
+        //let f = target().await;
+        let elapsed = now_nanos() - now;
+        // TODO: not sure how to see output -- info! compiles and runs but i don't know where it goes to under wasm-pack test
+        panic!("took {:.3}s", elapsed as f64 / 1e9);
+    }
 }
--- tests/wasm.rs
+++ tests/wasm.rs
@@ -7,7 +7,8 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 async fn test_bench() {
-    bench::drive(bench::snooze()).await;
+    console_error_panic_hook::set_once();
+    bench::drive(&bench::snooze).await;
 }
 
 #[wasm_bindgen_test]
