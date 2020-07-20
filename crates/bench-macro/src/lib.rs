c2fbc46d6f5daa953829cad56d16ae2395fe22c8
Nate White <whiten@georgetown.edu>
2020-07-20 13:08:14 -0700

    Code review feedback.

--- crates/bench-macro/src/lib.rs
+++ crates/bench-macro/src/lib.rs
@@ -1,5 +1,29 @@
-//! See the README for `wasm-bindgen-test` for a bit more info about what's
-//! going on here.
+// This source file is based on
+// https://github.com/rustwasm/wasm-bindgen/blob/master/crates/test/src/lib.rs,
+// which is Copyright (c) 2014 Alex Crichton, and is dual licensed under the
+// MIT and Apache licenses. Various changes have been made to adapt that code
+// to the use case here of supporting asynchronous benchmarks under wasm, and
+// all such changes are copyrighted and licensed separately.
+//
+// Text of the MIT license from wasm-bindgen, for completeness:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 
 extern crate proc_macro;
 
